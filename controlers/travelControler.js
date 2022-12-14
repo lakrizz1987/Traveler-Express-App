4ec63df0d499cf52179cf98b806dad890376f6c9
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-14 15:22:45 +0200

    crate pages

--- controlers/travelControler.js
+++ controlers/travelControler.js
@@ -6,5 +6,21 @@ router.get('/', (req, res) => {
     res.render('home')
 })
 
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.get('/create', (req, res) => {
+    res.render('create')
+})
+
+router.get('/trips', (req, res) => {
+    res.render('trips')
+})
+
 
 module.exports = router;
