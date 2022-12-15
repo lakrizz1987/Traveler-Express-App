54f97e03ac8992b877340cdb503e1584beed17ba
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-15 12:43:39 +0200

    create tripModel.js

---
+++ models/tripModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const TripSchema = new mongoose.Schema({
+    id: mongoose.Types.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/
+    },
+    creator:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Trip', TripSchema);
